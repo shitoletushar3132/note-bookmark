76ee9f5eabbf0cd6d9feebed246b6982ef23001a
Tushar shitole <153802284+shitoletushar3132@users.noreply.github.com>
2025-08-12 14:06:59 +0530

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://note-bookmark.onrender.com/api",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
